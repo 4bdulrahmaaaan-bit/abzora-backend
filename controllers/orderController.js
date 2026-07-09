fb39c5c752b2cb7847f8462e7e2a3a3ac90b56f7
4bdulrahmaaaan <4bdulrahmaaaan@gmail.com>
2026-07-09 20:25:35 +0530

    Fix checkout orderController import

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -19,6 +19,7 @@ const { generatePremiumInvoicePdf } = require('../services/invoicePdfService');
 const { enqueueInvoiceJob } = require('../services/invoiceService');
 const { recordTrackingEvent } = require('../services/trackingEventService');
 const { deliveryCheck } = require('../services/hyperlocalDeliveryService');
+const { enableLocalRiderDelivery } = require('../services/deliveryModeService');
 const { calculateOrderPricing, toPricingEngineConfig } = require('../services/pricingService');
 const { getPricingConfig } = require('../services/pricingConfigService');
 const {
